b54770cd391b9eff685c2e0df179cfbdb12653ba
Hirofumi Wakasugi <333180+5t111111@users.noreply.github.com>
2024-10-14 00:24:24 +0900

    update rust-extra

--- src/rust-extra/install.sh
+++ src/rust-extra/install.sh
@@ -6,7 +6,7 @@ set -e
 curl -L --proto '=https' --tlsv1.2 -sSf https://raw.githubusercontent.com/cargo-bins/cargo-binstall/main/install-from-binstall-release.sh | bash
 
 # Install Cargo binaries
-cargo binstall cargo-audit --locked
-cargo binstall cargo-edi --locked
-cargo binstall cargo-expan --locked
-cargo binstall cargo-watc --locked
+cargo binstall cargo-audit --force --locked
+cargo binstall cargo-edit --force --locked
+cargo binstall cargo-expand --force --locked
+cargo binstall cargo-watch --force --locked
